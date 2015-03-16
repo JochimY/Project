eafb7b0dd36556a5adcd7c90fde86c2fc0c5463b
923226138@qq.com <890322Xc>
2015-03-16 17:35:46 +0800

    a

--- src/com/example/wangluoxueyuan/MainActivity.java
+++ src/com/example/wangluoxueyuan/MainActivity.java
@@ -1,15 +1,30 @@
 package com.example.wangluoxueyuan;
 
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+	private Button button1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		button1=(Button)findViewById(R.id.button1);
+		button1.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(MainActivity.this, "aaaaaaaa",3000).show();
+			}
+		});
 	}
 
 	@Override
