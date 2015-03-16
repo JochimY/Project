c48035a88c5eb5b8b0cbe3a357a1c318a3a02167
923226138@qq.com <890322Xc>
2015-03-16 18:34:59 +0800

    分包

--- gen/com/example/wangluoxueyuan/R.java
+++ gen/com/example/wangluoxueyuan/R.java
@@ -24,12 +24,24 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int button1=0x7f080001;
-        public static final int textView1=0x7f080000;
+        public static final int action_settings=0x7f080008;
+        public static final int bookB=0x7f080003;
+        public static final int informationB=0x7f080004;
+        public static final int main_bottomLL=0x7f080000;
+        public static final int messageB=0x7f080001;
+        public static final int mineB=0x7f080005;
+        public static final int smallclassB=0x7f080002;
+        public static final int textView1=0x7f080007;
+        public static final int viewpager=0x7f080006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int book_recommend=0x7f030001;
+        public static final int information_informationlist=0x7f030002;
+        public static final int login_login=0x7f030003;
+        public static final int message_messagelist=0x7f030004;
+        public static final int mine_personalspace=0x7f030005;
+        public static final int smallclass_classification=0x7f030006;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- src/com/example/wangluoxueyuan/MainActivity.java
+++ src/com/example/wangluoxueyuan/MainActivity.java
@@ -1,30 +1,88 @@
 package com.example.wangluoxueyuan;
 
 
+import java.util.ArrayList;
+import com.example.wangluoxueyuan.frame.BaseActivity;
+import com.example.wangluoxueyuan.information.ui.Information_InformationList;
+import com.example.wangluoxueyuan.message.ui.Message_MessageList;
+import com.example.wangluoxueyuan.mine.ui.Mine_PersonalSpace;
+import com.example.wangluoxueyuan.smallclass.ui.SmallClass_Classification;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.View;
-import android.view.View.OnClickListener;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
-import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends BaseActivity {
+	public static ViewPager mPager;
+	
+	private ArrayList<Fragment> fragmentList;
+	private Message_MessageList message;
+	private Mine_PersonalSpace mine;
+	private Information_InformationList infomInformation;
+	private SmallClass_Classification smallClass;
+	private Information_InformationList book;
 
-	private Button button1;
+	private Button messageB;
 
+	private Button smallclassB;
+
+	private Button bookB;
+
+	private Button informationB;
+
+	private Button mineB;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		button1=(Button)findViewById(R.id.button1);
-		button1.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Toast.makeText(MainActivity.this, "aaaaaaaa",3000).show();
-			}
-		});
+		
+		
+		messageB=(Button)findViewById(R.id.messageB);
+		smallclassB=(Button)findViewById(R.id.smallclassB);
+		bookB=(Button)findViewById(R.id.bookB);
+		informationB=(Button)findViewById(R.id.informationB);
+		mineB=(Button)findViewById(R.id.mineB);
+		
+		messageB.setOnClickListener(new swithTo(0));
+		smallclassB.setOnClickListener(new swithTo(1));
+		bookB.setOnClickListener(new swithTo(2));
+		informationB.setOnClickListener(new swithTo(3));
+		mineB.setOnClickListener(new swithTo(4));
+		
+		
+
+		mPager = (ViewPager) findViewById(R.id.viewpager);
+		
+		
+		fragmentList=new ArrayList<Fragment>();
+		
+		message=Message_MessageList.getInstance(this);
+		smallClass=SmallClass_Classification.getInstance(this);
+		book=Information_InformationList.getInstance(this);
+		infomInformation=Information_InformationList.getInstance(this);
+		mine=Mine_PersonalSpace.getInstance(this);
+		
+		fragmentList.add(message);
+		fragmentList.add(smallClass);
+		fragmentList.add(book);
+		fragmentList.add(infomInformation);
+		fragmentList.add(mine);
+		// 给ViewPager设置适配器
+		mPager.setOffscreenPageLimit(5);
+		mPager.setAdapter(new MyFragmentPagerAdapter(
+				getSupportFragmentManager(), fragmentList));
+		mPager.setCurrentItem(0);// 设置当前显示标签页为第一页
+		
 	}
 
 	@Override
@@ -34,4 +92,60 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	class MyFragmentPagerAdapter extends FragmentPagerAdapter{
+		ArrayList<Fragment> list;
+		public MyFragmentPagerAdapter(FragmentManager fm,ArrayList<Fragment> list) {
+			super(fm);
+			this.list = list;
+			
+		}
+		
+		
+		@Override
+		public int getCount() {
+			return list.size();
+		}
+		
+		@Override
+		public Fragment getItem(int arg0) {
+			return list.get(arg0);
+		}
+		
+		
+		
+		
+	}
+	public class swithTo implements View.OnClickListener {
+		private int index = 0;
+
+		public swithTo(int i) {
+			index = i;
+		}
+
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+
+			bottomSet(index);
+
+		}
+	}
+	
+	public void bottomSet(int i) {
+		mPager.setCurrentItem(i);
+		switch (i) {
+		case 0:
+			break;
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		default:
+			break;
+		}
+	}
 }
